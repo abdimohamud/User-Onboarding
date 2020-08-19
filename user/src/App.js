4873093871c806c0a28087e38db5188e33fc5698
unknown <abdinajibmohamud@gmail.com>
2020-08-19 15:46:06 -0700

    finished with MVP

--- user/src/App.js
+++ user/src/App.js
@@ -1,6 +1,10 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Form from "./Components/Form"
 import './App.css';
+import axios from 'axios'
+import * as yup from 'yup'
+import Input from './Components/Input'
+import formSchema from './Validation/formSchema'
 
 
 
@@ -13,16 +17,46 @@ const defaultState = {
 };
 
 function App() {
-  const [formState, setFormState] = useState(defaultState)
-  const [formValues, setFormValues] = useState(defaultState)
+  const [friends, setFriends] = useState(initialFriends)          // array of friend objects
+  const [formValues, setFormValues] = useState(initialFormValues) // object
+  const [formErrors, setFormErrors] = useState(initialFormErrors) // object
+  const [disabled, setDisabled] = useState(initialDisabled)
 
+  const getFriends = () => {
+    axios.get('https://reqres.in/api/users')
+      .then(res => {
+        setFriends(res.data.data)
+      })
+      .catch(err => {
+        debugger
+      })
+  }
 
+  const postNewFriend = newFriend => {
+     axios.post('https://reqres.in/api/user', newFriend)
+      .then(res => {
+        
+        setFriends([...friends, .res.data])
+      })
+      .catch(err => {
+        debugger
+      })
+      .finally(() => {
+        setFormValues(initialFormValues)
+      })
+  }
   const inputInfo = (name, value) => {
+    yup 
+      .reach(formSchema, name)
+      .validate(value)
+      .then(valid
     setFormValues({
       ...formValues,
       [name]: value
     })
-  }
+    
+
+  })
 
   const submit = () => {
     // const newFriend = {
